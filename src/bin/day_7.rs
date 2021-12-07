2825d1e7347e2ca8686bf8c7b87cd1e8622f91ce
mrtims <54707912+mrtims@users.noreply.github.com>
2021-12-07 18:22:13 +1300

    Day 7: Crab dance (saved by the optimiser)

---
+++ src/bin/day_7.rs
@@ -0,0 +1,59 @@
+use advent_2021::read_lines;
+
+#[test]
+fn example_1() {
+    let crabs = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+    let cost = (0..=16).map(|n| cost(n, &crabs)).min();
+    assert_eq!(Some(37), cost);
+}
+
+fn cost(n: i32, crabs: &[i32]) -> i32 {
+    crabs.iter().map(|crab| i32::abs(*crab - n)).sum()
+}
+
+#[test]
+fn example_2() {
+    let crabs = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+    let cost = (0..=16).map(|n| cost_2(n, &crabs)).min();
+    assert_eq!(Some(168), cost);
+}
+
+fn cost_2(n: i32, crabs: &[i32]) -> i32 {
+    crabs
+        .iter()
+        .map(|crab| (0..=i32::abs(*crab - n)).sum::<i32>())
+        .sum()
+}
+
+fn read_input_file(filename: &str) -> Vec<i32> {
+    if let Ok(lines) = read_lines(filename) {
+        let first: String = lines
+            .take(1)
+            .filter_map(|x| x.ok())
+            .collect::<Vec<String>>()[0]
+            .clone();
+        first
+            .split(',')
+            .filter_map(|n| str::parse::<i32>(n).ok())
+            .collect()
+    } else {
+        Vec::new()
+    }
+}
+
+fn main() {
+    let crabs = read_input_file("input/day_7.txt");
+    let min = crabs.iter().min().unwrap();
+    let max = crabs.iter().max().unwrap();
+    // part 1:
+    {
+        let result = (*min..=*max).map(|n| cost(n, &crabs)).min().unwrap();
+        println!("#1: {}", result);
+    }
+
+    // part 2:
+    {
+        let result = (*min..=*max).map(|n| cost_2(n, &crabs)).min().unwrap();
+        println!("#2: {}", result);
+    }
+}
